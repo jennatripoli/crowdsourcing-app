452c47b74ec56405c2024e8af2e7865400a369ea
Jenna Tripoli <jennamt1282@gmail.com>
2022-11-29 16:33:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import DesignerViewProject from './pages/DesignerViewProject';
+import DesignerViewProject from './boundary/DesignerViewProject';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
