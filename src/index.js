924b804e5a43765afd9b087b15369484e2d7b2fe
Jenna Tripoli <jennamt1282@gmail.com>
2022-11-29 13:52:54 -0500

    Basic layout

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import DesignerViewProject from './pages/DesignerViewProject';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DesignerViewProject />
   </React.StrictMode>
 );
 
