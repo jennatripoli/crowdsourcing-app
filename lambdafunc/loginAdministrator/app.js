aa483311267c551217e5b2ea44a271705b1684c0
Ryan Dillon <rjdillon@wpi.edu>
2022-11-29 19:26:46 +0000

    updated loginAdmin function

--- lambdafunc/loginAdministrator/app.js
+++ lambdafunc/loginAdministrator/app.js
@@ -1,7 +1,45 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 let response;
-// const mysql = require('mysql');
+const mysql = require('mysql');
+
+var config = require('./config.json');
+
+var pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+function query(conx, sql, params) {
+    return new Promise((resolve, reject) => {
+        conx.query(sql, params, function(err, rows) {
+            if (err) {
+                // reject because there was an error
+                reject(err);
+            } else {
+                // resolve because we have result(s) from the query. it may be an empty rowset or contain multiple values
+                resolve(rows);
+            }
+        });
+    });
+}
+
+
+let getAllProjects = () => {
+    return new Promise((resolve, reject) => {
+                pool.query("SELECT * FROM Project", (error, rows) => {
+                    if (error) { return reject(error); }
+                    if (rows) {
+                        return resolve(rows);
+                    } else {
+                        return reject("there are no projects");
+                    }
+                });
+            });
+
+}
 
 /**
  *
@@ -41,11 +79,37 @@ exports.lambdaHandler = async (event, context) => {
         // have to add database stuff here
         // return on 200 list of all projects ever
         
+        // {“projects” : [{"name” : “project1”, “description” : “this project is”,  
+
+        // “entrepreneur” : “xxx@gmail.com”, “type” : “film”, “goal” : 1000,  
+
+        // “deadline” : “12-01-2022", “active-pledges” : [{...}, …]. “direct-supports” : [{...}, ...],  
+
+        // “successful” : false, “launched” : false}, …]} 
+        
         // const ret = await axios(url);
+        let projects = getAllProjects();
+        let list = [];
+        
+        for (let i = 0; i<projects.length; i++) {
+            let project = projects[i];
+            list[i] = {
+                name: project.name,
+                description: project.story,
+                entrepreneur: project.designerEmail,
+                type: project.type,
+                goal: project.goal,
+                deadline: project.deadline,
+                successful: project.successful,
+                launched: project.launched
+            };
+        };
+        
         response.statusCode = 200;
+        
         response.body  = JSON.stringify({
-            projects: []
-        })
+            list,
+        });
         
     } catch (err) {
         console.log(err);
