2504c674d8ad23fb74efa31c23b534b8159e1b64
Ryan Dillon <rjdillon@wpi.edu>
2022-12-01 02:26:47 +0000

    added loginDesigner

--- lambdafunc/loginDesigner/app.js
+++ lambdafunc/loginDesigner/app.js
@@ -1,7 +1,31 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 let response;
-// const mysql = require('mysql');
+const mysql = require('mysql');
+
+var config = require('./config.json');
+
+var pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+function query(conx, sql, params) {
+    return new Promise((resolve, reject) => {
+        conx.query(sql, params, function(err, rows) {
+            if (err) {
+                // reject because there was an error
+                reject(err);
+            } else {
+                // resolve because we have result(s) from the query. it may be an empty rowset or contain multiple values
+                resolve(rows);
+            }
+        });
+    });
+}
+
 
 /**
  *
@@ -29,23 +53,93 @@ exports.lambdaHandler = async (event, context) => {
     }; // response
 
 
-    let actual_event = event.body;
-    let info = JSON.parse(actual_event);
-    console.log("info:" + JSON.stringify(info)); //  info.arg1 and info.arg2
+    // let actual_event = event.body;
+    //let info = JSON.parse(event);
+    console.log("info:" + JSON.stringify(event)); //  info.arg1
+    
+    //RYAN
+    /*let getAllProjects = () => {
+    return new Promise((resolve, reject) => {
+                pool.query("SELECT * FROM Project", (error, rows) => {
+                    if (error) { return reject(error); }
+                    if (rows) {
+                        return resolve(rows);
+                    } else {
+                        return reject("there are no projects");
+                    }
+                });
+            });
+
+    };*/
+    
+    //MIKAELA
+    let GetValidUser = (email) => {
+    return new Promise((resolve, reject) => {
+                pool.query("SELECT * FROM ProjectDesigner WHERE email=?", [email], (error, rows) => {
+                    if (error) { return reject(error); }
+                    if ((rows) && (rows.length == 1)) {
+                        return resolve(rows[0].email);
+                    } else {
+                        return reject("unable to locate user '" + email + "'");
+                    }
+
+                });
+            });
+    };
+
 
     try {
         // DATABASE STUFF HERE
         
         // receive a POST    
-        // {“type” : “designer”, “email” : “xxx@gmail.com”,  “password” : “xxx”} 
+        // {“type” : “administrator”, “email” : “xxx@gmail.com”,  “password” : “xxx”} 
         // have to add database stuff here
         // return on 200 list of all projects ever
         
+        // {“projects” : [{"name” : “project1”, “description” : “this project is”,  
+
+        // “entrepreneur” : “xxx@gmail.com”, “type” : “film”, “goal” : 1000,  
+
+        // “deadline” : “12-01-2022", “active-pledges” : [{...}, …]. “direct-supports” : [{...}, ...],  
+
+        // “successful” : false, “launched” : false}, …]} 
+        
         // const ret = await axios(url);
+        
+        //RYAN
+        /* let projects = getAllProjects();
+        //let list = [];
+        
+        for (let i = 0; i<projects.length; i++) {
+            let project = projects[i];
+            list[i] = {
+                name: project.name,
+                description: project.story,
+                entrepreneur: project.designerEmail,
+                type: project.type,
+                goal: project.goal,
+                deadline: project.deadline,
+                successful: project.successful,
+                launched: project.launched
+            };
+        };
+        
         response.statusCode = 200;
+        
         response.body  = JSON.stringify({
-            projects: []
-        })
+            list,
+        });
+        */
+        
+        //MIKAELA
+        const arg1_value = await GetValidUser(event.arg1);
+        
+        // If either is NaN then there is an error
+        response.statusCode = 200;
+        let result = arg1_value;
+        response.result = result.toString();
+
+        
     } catch (err) {
         console.log(err);
         return err;
