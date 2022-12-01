06231abd5403f94f1e99d732b61fa9d6f8ca448e
EC2 Default User <ec2-user@ip-172-31-2-110.ec2.internal>
2022-12-01 16:21:18 +0000

    pushing functional designerList -MM

--- lambdafunc/designerList/app.js
+++ lambdafunc/designerList/app.js
@@ -1,7 +1,31 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 let response;
-// const mysql = require('mysql');
+const mysql = require('mysql');
+
+var config = require('./config.json');
+
+var pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+function query(conx, sql, params) {
+    return new Promise((resolve, reject) => {
+        conx.query(sql, params, function(err, rows) {
+            if (err) {
+                // reject because there was an error
+                reject(err);
+            } else {
+                // resolve because we have result(s) from the query. it may be an empty rowset or contain multiple values
+                resolve(rows);
+            }
+        });
+    });
+}
+
 
 /**
  *
@@ -28,26 +52,71 @@ exports.lambdaHandler = async (event, context) => {
         }
     }; // response
 
+    
+    let getDesignerProjects = (email) => {
+    return new Promise((resolve, reject) => {
+                pool.query("SELECT * FROM Project WHERE designerEmail=?", [email], (error, rows) => {
+                    if (error) { return reject(error); }
+                    if (rows) {
+                        return resolve(rows);
+                    } else {
+                        return reject("there are no projects");
+                    }
+                });
+            });
 
-    let actual_event = event.body;
-    let info = JSON.parse(actual_event);
-    console.log("info:" + JSON.stringify(info)); //  info.arg1 and info.arg2
+    };
 
-    try {
+   
+   try {
         // DATABASE STUFF HERE
         
         // receive a POST    
-        // {“email” : “xxx@gmail.com”} 
+        // {“type” : “administrator”, “email” : “xxx@gmail.com”,  “password” : “xxx”} 
         // have to add database stuff here
-        // return on 200 list of all designers projects
+        // return on 200 list of all projects ever
+        
+        // {“projects” : [{"name” : “project1”, “description” : “this project is”,  
+
+        // “entrepreneur” : “xxx@gmail.com”, “type” : “film”, “goal” : 1000,  
+
+        // “deadline” : “12-01-2022", “active-pledges” : [{...}, …]. “direct-supports” : [{...}, ...],  
+
+        // “successful” : false, “launched” : false}, …]} 
         
         // const ret = await axios(url);
-        response.statusCode = 200;
-        response.body  = JSON.stringify({
-            //list all designers projects
-        })
+        let projects = await getDesignerProjects(event.arg1);
         
-    } catch (err) {
+        if(projects) {
+            
+            let list = [];
+            
+            console.log("info: " + JSON.stringify(projects));
+            
+            for (let i = 0; i<projects.length; i++) {
+                let project = projects[i];
+                list[i] = {
+                    name: project.name,
+                    description: project.story,
+                    entrepreneur: project.designerEmail,
+                    type: project.type,
+                    goal: project.goal,
+                    deadline: project.deadline,
+                    successful: project.successful,
+                    launched: project.launched
+                };
+            };
+            response.statusCode = 200;
+            response.result  = JSON.stringify({list,});
+        }else {
+            response.statusCode = 400;
+            response.error = "Couldn't find projects ";
+        }
+        
+        
+        
+    } 
+    catch (err) {
         console.log(err);
         return err;
     }
